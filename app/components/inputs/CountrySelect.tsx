1bce233ac88cadcb90d9e059744e21f766cdde13
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-12-03 03:59:38 -0300

    deploy fix

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -27,9 +27,12 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
         isClearable
         options={getAll()}
         value={value}
-        onChange={(value: CountrySelectValue) => onChange(value)}
-        formatOptionLabel={(option: CountrySelectValue) => (
-          <div className="flex flex-row items-center gap-3">
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div
+            className="
+          flex flex-row items-center gap-3"
+          >
             <div>{option.flag}</div>
             <div>
               {option.label},<span className="text-neutral-500 ml-1">{option.region}</span>
@@ -41,7 +44,7 @@ const CountrySelect: React.FC<CountrySelectProps> = ({ value, onChange }) => {
           input: () => 'text-lg',
           option: () => 'text-lg'
         }}
-        theme={(theme: any) => ({
+        theme={(theme) => ({
           ...theme,
           borderRadius: 6,
           colors: {
